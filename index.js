16f0cc90748c0532965efa1734ef1fc1aa96d3de
Leomaia07 <leonardo.maia07@gmail.com>
2024-12-16 17:01:05 -0400

    entrega de desafio

---
+++ index.js
@@ -0,0 +1,46 @@
+class hero {
+    constructor(name, age, type){
+        this.name = name;
+        this.age = age;
+        this.type = type;
+    }
+
+    atacar() {
+        let ataque = this.type
+        
+        switch(true) {
+            case ataque == "mago":
+                console.log("O " + this.type + " atacou usando magia");
+                break
+            case ataque == "guerreiro":
+                console.log("O " + this.type + " atacou usando espada");
+                break
+            case ataque == "monge":
+                console.log("O " + this.type + " atacou usando artes marciais");
+                break
+            case ataque == "ninja":
+                console.log("O " + this.type + " atacou usando shuriken");
+                break
+            default:
+                console.log("não encontrado");
+        }
+
+    }
+
+}
+let hero1 = new hero("jose", 23, "leo")
+
+hero1.atacar()
+
+        
+
+
+
+
+
+
+ 
+
+
+
+
